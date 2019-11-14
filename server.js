236e0bd3af5166cf8175296d475c326601749411
Findlay Smith <findlayksmith@gmail.com>
2019-11-14 03:20:00 +0000

    apply authentication to videos

--- server.js
+++ server.js
@@ -1,102 +1,102 @@
-const express = require('express')
-const fs      = require('fs')
-const config  = require('config')
-const session = require('express-session')
-const app     = express()
-const http    = require('http').createServer(app)
-const crypto  = require('crypto')
-const folder  = require('./folder')
-const io      = require('socket.io')(http)
-const vstore  = require('./vstore.js')
+const express = require("express");
+const fs = require("fs");
+const config = require("config");
+const session = require("express-session");
+const app = express();
+const http = require("http").createServer(app);
+const crypto = require("crypto");
+const folder = require("./folder");
+const io = require("socket.io")(http);
+const vstore = require("./vstore.js");
 
+let ensureAuth = (req, res, next) => {
+    if (!req.session || !req.session.authenticated) {
+        res.redirect("/");
+        return;
+    }
+    next();
+};
 // preferable use nginx/similar in production
-app.use('/static/videos', express.static(config.videos_folder))
-app.use('/static', express.static('static'))
-
+app.use("/static/videos", ensureAuth, express.static(config.videos_folder));
+app.use("/static", express.static("static"));
 
 // set up sessions
-app.use(session({
-    secret: crypto.randomBytes(64).toString(),
-    cookie: { expires: 12 * 60 * 60 * 1000 } // 12 hours
-}))
-let ensureAuth = (req, res, next) => {
-    if (!req.session.authenticated) {
-        res.redirect('/')
-        return
-    }
-    next()
-}
-
+app.use(
+    session({
+        secret: crypto.randomBytes(64).toString(),
+        cookie: { expires: 12 * 60 * 60 * 1000 } // 12 hours
+    })
+);
 
 // set up rendering
-app.set('views', './views')
-app.set('view engine', 'ejs')
+app.set("views", "./views");
+app.set("view engine", "ejs");
 
-app.get('/', (req, res) => {
-    res.render('pages/index')
-})
+app.get("/", (req, res) => {
+    res.render("pages/index");
+});
 
-app.get('/videos/', ensureAuth, (req, res) => {
+app.get("/videos/", ensureAuth, (req, res) => {
     folder.listAllVideos().then(videos => {
-        res.render('pages/videos', { videos })
-    })
-})
+        res.render("pages/videos", { videos });
+    });
+});
 
-app.get('/watch/*', ensureAuth, (req, res) => {
-    const videoPath = decodeURI(req.path.substr('/watch/'.length))
+app.get("/watch/*", ensureAuth, (req, res) => {
+    const videoPath = decodeURI(req.path.substr("/watch/".length));
 
-    res.render('pages/viewer', { videoPath })
-})
+    res.render("pages/viewer", { videoPath });
+});
 
 // this is temporary, but *fuck it*
-const videoStore = new vstore.VideoStore()
-io.on('connection', socket => {
-    console.log('socket connected')
-    socket.on('watching', msg => {
-        socket.join(msg)
-        videoStore.addViewer(msg, socket)
-        console.log(`viewers now for ${msg}: ${videoStore.getViewerCount(msg)}`)
-        socket.broadcast.to(msg).emit('viewers', videoStore.getViewerCount(msg))
-        socket.emit('viewers', videoStore.getViewerCount(msg))
-        videoStore.initiateSync(msg, socket)
-    })
+const videoStore = new vstore.VideoStore();
+io.on("connection", socket => {
+    console.log("socket connected");
+    socket.on("watching", msg => {
+        socket.join(msg);
+        videoStore.addViewer(msg, socket);
+        console.log(`viewers now for ${msg}: ${videoStore.getViewerCount(msg)}`);
+        socket.broadcast.to(msg).emit("viewers", videoStore.getViewerCount(msg));
+        socket.emit("viewers", videoStore.getViewerCount(msg));
+        videoStore.initiateSync(msg, socket);
+    });
 
-    socket.on('pause', msg => {
-        console.log('broadcasting pause')
-        socket.broadcast.to(msg).emit('pause')
-    })
+    socket.on("pause", msg => {
+        console.log("broadcasting pause");
+        socket.broadcast.to(msg).emit("pause");
+    });
 
-    socket.on('play', msg => {
-        console.log('broadcasting play')
-        socket.broadcast.to(msg).emit('play')
-    })
+    socket.on("play", msg => {
+        console.log("broadcasting play");
+        socket.broadcast.to(msg).emit("play");
+    });
 
-    socket.on('seek', msg => {
-        socket.broadcast.to(msg.video).emit('seek', {timestamp: msg.timestamp})
-    })
+    socket.on("seek", msg => {
+        socket.broadcast.to(msg.video).emit("seek", { timestamp: msg.timestamp });
+    });
 
-    socket.on('disconnect', () => {
-        console.log('socket disconnected')
-        videoStore.removeViewer(socket)
+    socket.on("disconnect", () => {
+        console.log("socket disconnected");
+        videoStore.removeViewer(socket);
         for (let room in socket.rooms) {
-            console.log('removing from: room')
-            socket.broadcast.to(room).emit('viewers', videoStore.getViewerCount(room))
+            console.log("removing from: room");
+            socket.broadcast.to(room).emit("viewers", videoStore.getViewerCount(room));
         }
-    })
-})
+    });
+});
 
-app.get('/api/login', (req, res) => {
+app.get("/api/login", (req, res) => {
     // this is not super safe
     if (req.query.pw === config.password) {
-        req.session.authenticated = true
+        req.session.authenticated = true;
         res.json({
             success: true
-        })
-        return
+        });
+        return;
     }
     res.json({
         success: false
-    })
-})
+    });
+});
 
-http.listen(config.port, () => console.log(`listening on *:${config.port}`))
+http.listen(config.port, () => console.log(`listening on *:${config.port}`));
